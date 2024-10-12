be7825fd8aa125d6d2cde26f7d229b7e57f0bb5e
Angel <luisangel19154@gmail.com>
2024-10-11 19:23:14 -0500

    first commit

---
+++ src/main/java/com/serviceSoap/T2_ServiceSoapLuisGrados/T2ServiceSoapLuisGradosApplication.java
@@ -0,0 +1,13 @@
+package com.serviceSoap.T2_ServiceSoapLuisGrados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2ServiceSoapLuisGradosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2ServiceSoapLuisGradosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serviceSoap/T2_ServiceSoapLuisGrados/T2ServiceSoapLuisGradosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceSoap.T2_ServiceSoapLuisGrados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2ServiceSoapLuisGradosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
